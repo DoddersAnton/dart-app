80569a40f00f91027433e13e768d9ac60793fb84
Anthony Dodwell <anthony_dodwell@hotmail.com>
2026-03-29 12:58:34 +0100

    logo and ico change

--- components/nav/logo.tsx
+++ components/nav/logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
 
   return (
     <div className="flex items-center gap-2 cursor-pointer md:cursor-default">
-      <Image src="/darts-logo.png" alt="logo" width={42} height={24} />
+      <Image src="/DRT-logo.png" alt="logo" width={42} height={24} />
       <div className="text-sm font-bold text-[14px]">Dartiau Rhieni Trisant</div>
     </div>
   );
